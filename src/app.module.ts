47a804f87e44278585b216022eaf51ddc81ab2c3
Sebastián Lozano <sebastianl2.dev@gmail.com>
2025-12-28 14:25:52 -0500

    config: Set pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,19 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            messageKey: 'message',
+          },
+        },
+        messageKey: 'message',
+      },
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
 
 async function berryMix() {
   const app = await NestFactory.create(AppModule);
+
+  app.useLogger(app.get(Logger));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
