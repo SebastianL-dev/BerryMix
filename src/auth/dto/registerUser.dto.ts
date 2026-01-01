47887d6ed512176961b99b80e2bd9e95bd9efb42
Sebastián Lozano <sebastianl2.dev@gmail.com>
2026-01-01 18:55:39 -0500

    feat: Add register user dto
    Create dto and validate every field in user dto, fix eslint config to
    avoid unused variables that starts wiht underscore.

---
+++ src/auth/dto/registerUser.dto.ts
@@ -0,0 +1,51 @@
+import {
+  IsBoolean,
+  IsDate,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  MinLength,
+} from 'class-validator';
+
+export class RegisterUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(2)
+  password_hash: string;
+
+  @IsUrl()
+  avatar_url: string | null;
+
+  @IsString()
+  @IsOptional()
+  role?: 'user' | 'admin' | 'premium_user';
+
+  @IsBoolean()
+  @IsOptional()
+  is_verified: boolean;
+
+  @IsBoolean()
+  @IsOptional()
+  is_active: boolean;
+
+  @IsDate()
+  @IsOptional()
+  created_at: Date | null;
+
+  @IsDate()
+  @IsOptional()
+  updated_at: Date | null;
+
+  @IsDate()
+  @IsOptional()
+  last_login_at: Date | null;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
+import { ValidationPipe } from '@nestjs/common';
 
 async function berryMix() {
   const app = await NestFactory.create(AppModule, { logger: false });
 
   app.useLogger(app.get(Logger));
   app.enableCors('*');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
