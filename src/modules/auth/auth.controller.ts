0293f35c44ebaad6263931ae4da643f71acb6ff3
Sebastián Lozano <sebastianl2.dev@gmail.com>
2026-01-04 21:46:09 -0500

    feat: Add refresh tokes to improve security
    Create refresh endopoint to verify an active refresh token, validate it,
    revoke it and then generate a new access and refresh tokens, improve
    endpoints and rotate refresh tokens.

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,30 +1,24 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
-import type { Response } from 'express';
+import type { Response, Request } from 'express';
 import { Public } from 'src/common/decorators/public.decorator';
-import { ConfigService } from '@nestjs/config';
+import { CookieService } from 'src/common/security';
 
 @Controller('auth')
 export class AuthController {
-  private readonly nodeEnv: string;
-
   constructor(
     private authService: AuthService,
-    private readonly configService: ConfigService,
-  ) {
-    this.nodeEnv = this.configService.get<string>('env.node')!;
-  }
-
-  private setCookie(response: Response, token: string) {
-    response.cookie('berrymix_acc_token', token, {
-      httpOnly: true,
-      secure: this.nodeEnv === 'production',
-      sameSite: 'strict',
-      maxAge: 1000 * 60 * 30,
-    });
-  }
+    private cookieService: CookieService,
+  ) {}
 
   @Public()
   @Post('register')
@@ -32,9 +26,22 @@ export class AuthController {
     @Body() registerUserDto: RegisterUserDto,
     @Res({ passthrough: true }) response: Response,
   ) {
-    const { user, token } = await this.authService.register(registerUserDto);
+    const { user, accessToken, refreshToken } =
+      await this.authService.register(registerUserDto);
 
-    this.setCookie(response, token);
+    this.cookieService.set(
+      response,
+      'berrymix_acc_token',
+      accessToken,
+      1000 * 60 * 30,
+    );
+    this.cookieService.set(
+      response,
+      'berrymix_ref_token',
+      refreshToken,
+      1000 * 60 * 60 * 24 * 7,
+      '/auth/refresh',
+    );
     return user;
   }
 
@@ -44,16 +51,72 @@ export class AuthController {
     @Body() loginUserDto: LoginUserDto,
     @Res({ passthrough: true }) response: Response,
   ) {
-    const { user, token } = await this.authService.login(loginUserDto);
+    const { user, accessToken, refreshToken } =
+      await this.authService.login(loginUserDto);
 
-    this.setCookie(response, token);
+    this.cookieService.set(
+      response,
+      'berrymix_acc_token',
+      accessToken,
+      1000 * 60 * 30,
+    );
+    this.cookieService.set(
+      response,
+      'berrymix_ref_token',
+      refreshToken,
+      1000 * 60 * 60 * 24 * 7,
+      '/auth/refresh',
+    );
+
     return user;
   }
 
   @Post('logout')
-  logout(@Res({ passthrough: true }) response: Response) {
-    response.clearCookie('berrymix_acc_token');
+  async logout(
+    @Req() request: { user: { user_id: string } },
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const userId = request.user.user_id;
+
+    if (userId) {
+      await this.authService.revokeAllUserTokens(userId);
+    }
+
+    this.cookieService.clear(response, 'berrymix_acc_token');
+    this.cookieService.clear(response, 'berrymix_ref_token', '/auth/refresh');
 
     return { message: 'Logged out succesfully' };
+  }
+
+  @Public()
+  @Post('refresh')
+  async refresh(
+    @Req() request: Request,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const refreshToken = request.cookies?.berrymix_ref_token as
+      | string
+      | undefined;
+
+    if (!refreshToken) throw new UnauthorizedException('Invalid credentials');
+
+    const { newAccessToken, newRefreshToken } =
+      await this.authService.refreshTokens(refreshToken);
+
+    this.cookieService.set(
+      response,
+      'berrymix_acc_token',
+      newAccessToken,
+      1000 * 60 * 30,
+    );
+    this.cookieService.set(
+      response,
+      'berrymix_ref_token',
+      newRefreshToken,
+      1000 * 60 * 60 * 24 * 7,
+      '/auth/refresh',
+    );
+
+    return { message: 'Token refreshed successfully' };
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -10,6 +10,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { HashingService, TokenService } from 'src/common/security/index';
+import { Prisma, RefreshToken } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
@@ -25,7 +26,7 @@ export class AuthService {
     const { password } = registerUserDto;
 
     try {
-      const hashedPassword = await this.hashingService.hash(password);
+      const hashedPassword = await this.hashingService.hashPassword(password);
 
       const newUser = await this.prisma.user.create({
         data: { ...registerUserDto, password: hashedPassword },
@@ -36,19 +37,26 @@ export class AuthService {
           avatar_url: true,
           role: true,
           is_verified: true,
-          created_at: true,
+          last_login_at: true,
         },
       });
 
-      const token = this.tokenService.signToken(
-        newUser.id,
-        newUser.role,
-        newUser.is_verified,
-      );
+      const accessToken = this.tokenService.signAccessToken(newUser.id);
+
+      const refreshToken = this.tokenService.refreshToken();
+      const hashedRefreshToken = this.hashingService.hashToken(refreshToken);
+
+      await this.prisma.refreshToken.create({
+        data: {
+          user_id: newUser.id,
+          token_hash: hashedRefreshToken,
+          expires_at: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+        },
+      });
 
       this.logger.log('User registered sucessfully');
 
-      return { user: newUser, token };
+      return { user: newUser, accessToken, refreshToken };
     } catch (error) {
       if ((error as { code: string }).code === 'P2002')
         throw new ConflictException('This email is already in use');
@@ -71,7 +79,7 @@ export class AuthService {
       if (!foundUser || !foundUser.is_active)
         throw new UnauthorizedException('Invalid credentials');
 
-      const isValidPassword = await this.hashingService.compare(
+      const isValidPassword = await this.hashingService.comparePassword(
         password,
         foundUser.password,
       );
@@ -93,15 +101,107 @@ export class AuthService {
         },
       });
 
-      const token = this.tokenService.signToken(
-        updatedUser.id,
-        updatedUser.role,
-        updatedUser.is_verified,
-      );
+      const accessToken = this.tokenService.signAccessToken(updatedUser.id);
+
+      const refreshToken = this.tokenService.refreshToken();
+      const hashedRefreshToken = this.hashingService.hashToken(refreshToken);
+
+      await this.prisma.refreshToken.create({
+        data: {
+          user_id: updatedUser.id,
+          token_hash: hashedRefreshToken,
+          expires_at: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+        },
+      });
 
       this.logger.log(`User logged in succesfully: ${email}`);
 
-      return { user: updatedUser, token };
+      return { user: updatedUser, accessToken, refreshToken };
+    } catch (error) {
+      if (error instanceof HttpException) throw error;
+
+      this.logger.error(error);
+      throw new InternalServerErrorException('Something went wrong');
+    }
+  }
+
+  async refreshTokens(refreshToken: string) {
+    const hash = this.hashingService.hashToken(refreshToken);
+
+    try {
+      return this.prisma.$transaction(async (tx) => {
+        const token = await tx.refreshToken.findUnique({
+          where: {
+            token_hash: hash,
+          },
+        });
+
+        if (!token || token.is_revoked)
+          throw new UnauthorizedException('Invalid credentials');
+
+        if (new Date() > token.expires_at)
+          throw new UnauthorizedException('Invalid credentials');
+
+        const newAccessToken = this.tokenService.signAccessToken(token.user_id);
+        const newRefreshToken = await this.rotateToken(tx, token);
+
+        return { newAccessToken, newRefreshToken };
+      });
+    } catch (error) {
+      if (error instanceof HttpException) throw error;
+
+      this.logger.error(error);
+      throw new InternalServerErrorException('Something went wrong');
+    }
+  }
+
+  private async rotateToken(tx: Prisma.TransactionClient, token: RefreshToken) {
+    try {
+      const revoked = await tx.refreshToken.updateMany({
+        where: { id: token.id },
+        data: { is_revoked: true, revoked_date: new Date() },
+      });
+
+      if (revoked.count === 0) {
+        await tx.refreshToken.updateMany({
+          where: { user_id: token.user_id },
+          data: { is_revoked: true, revoked_date: new Date() },
+        });
+
+        throw new UnauthorizedException('Token reuse detected');
+      }
+
+      const newRefreshToken = this.tokenService.refreshToken();
+      const newHash = this.hashingService.hashToken(newRefreshToken);
+
+      await tx.refreshToken.create({
+        data: {
+          user_id: token.user_id,
+          token_hash: newHash,
+          expires_at: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+        },
+      });
+
+      return newRefreshToken;
+    } catch (error) {
+      if (error instanceof HttpException) throw error;
+
+      this.logger.error(error);
+      throw new InternalServerErrorException('Something went wrong');
+    }
+  }
+
+  async revokeAllUserTokens(userId: string) {
+    try {
+      await this.prisma.refreshToken.updateMany({
+        where: {
+          user_id: userId,
+        },
+        data: {
+          is_revoked: true,
+          revoked_date: new Date(),
+        },
+      });
     } catch (error) {
       if (error instanceof HttpException) throw error;
 
