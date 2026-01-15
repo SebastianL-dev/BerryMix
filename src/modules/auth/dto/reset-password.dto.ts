59dede5deb151b55b192ced0aff38dae2ab4f0cc
Sebastián Lozano <sebastianl2.dev@gmail.com>
2026-01-15 18:27:45 -0500

    feat: Create dtos to reset password

---
+++ src/modules/auth/dto/reset-password.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class ResetPasswordDto {
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(100)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/, {
+    message:
+      'Password must include uppercase and lowercase letters, a number, and a special character',
+  })
+  password: string;
+}
